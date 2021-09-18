3454399361512a7d0b5cb4b0c9d360b99f766b9e
Raul <raulrufino0@gmail.com>
2021-09-17 21:27:42 -0300

    feat: Add header of an implementation of doble linked list

---
+++ src/dlist.h
@@ -0,0 +1,39 @@
+#include <stdbool.h>
+
+typedef struct DoublyEndedList dlist_t;
+typedef struct DoublyEndedNode dnode_t;
+
+struct DoublyEndedList {
+    dnode_t* front;
+    dnode_t* rear;
+};
+
+struct DoublyEndedNode {
+    dnode_t* left;
+    void* element;
+    dnode_t* right;
+};
+
+dlist_t* alloc_dlist();
+
+dnode_t* alloc_dnode();
+
+void free_node(dnode_t* node);
+
+void free_list(dlist_t* list);
+
+bool is_empty(dlist_t* list);
+
+void append_right(dlist_t* list, void* element);
+
+void append_left(dlist_t* list, void* element);
+
+void set(dlist_t* list, int index, void* element);
+
+void* pop_right(dlist_t* list);
+
+void* pop_left(dlist_t* list);
+
+void* get(dlist_t* list, int index);
+
+
