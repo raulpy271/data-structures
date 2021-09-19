841282370b1c45e2411eb89b069ce4e661301bd3
Raul <raulrufino0@gmail.com>
2021-09-19 20:20:23 -0300

    feat: add implementation of a priority queue

---
+++ src/priority_queue.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+
+#include "priority_queue.h"
+
+node_t* alloc_node() {
+    queue_t node = malloc(sizeof (node_t));
+    node->element = NULL;
+    node->next = NULL;
+    return node;
+}
+
+bool is_empty(queue_t queue) {
+    return queue->element == NULL;
+}
+
+void pop_current(node_t* node) {
+    node_t* next = node->next;
+    node->element = next->element;
+    node->next = next->next;
+    free(next);
+}
+
+void* pop(queue_t* queue, bool (* compare_priority_of)(void*, void*)) {
+    node_t* higher_priority = *queue;
+    node_t* next = (*queue)->next;
+    void* popped_element = NULL;
+    while (!is_empty(next)) {
+        if (!compare_priority_of(higher_priority->element, next->element)) {
+            higher_priority = next;
+        }
+        next = next->next;
+    }
+    popped_element = higher_priority->element;
+    pop_current(higher_priority);
+    return popped_element;
+}
+
+void insert(queue_t* queue, void* element) {
+    node_t* node = alloc_node();
+    node->element = element;
+    node->next = *queue;
+    *queue = node;
+};
+
+
