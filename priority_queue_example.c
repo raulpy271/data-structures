c0e26d2d7468dac4c8b23a7d5bad40c48e19322a
Raul <raulrufino0@gmail.com>
2021-09-19 20:20:41 -0300

    feat: Add example using priority queue

---
+++ priority_queue_example.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#include "src/priority_queue.h"
+
+bool smallest_int(void* a, void* b) {
+    return (*(int *)a) < (*(int *)b);
+}
+
+void sort_arr_using_queue(queue_t* queue, int arr[]) {
+    int i = 0;
+    while(!is_empty(*queue)) {
+        arr[i] = *(int *)pop(queue, smallest_int);
+        i++;
+    }
+}
+
+int main() {
+    // Sort an array using a priority queue
+    int length = 4;
+    int sorted_arr[4] = {};
+    queue_t unsorted_queue = alloc_node();
+    int element1 = 10;
+    int element2 = 20;
+    int element3 = 30;
+    int element4 = 40;
+    insert(&unsorted_queue, &element4);
+    insert(&unsorted_queue, &element2);
+    insert(&unsorted_queue, &element1);
+    insert(&unsorted_queue, &element3);
+    sort_arr_using_queue(&unsorted_queue, sorted_arr);
+    printf("sorted arr: ");
+    for (int i = 0; i < length; i++) {
+        printf("%d ", sorted_arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
